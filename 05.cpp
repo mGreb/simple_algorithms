576a1d30b3e7fdbe3fe88ff18265339b22abe893
mgreb92 <mgreb92@gmail.com>
2025-03-26 16:04:42 +0300

    Solved queue+max problem

---
+++ 05.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <optional>
+
+class stack_with_max {
+private:
+    std::stack<std::pair<int, int>> data;  // { val, max }
+public:
+    void push(int val) {
+        data.push({val, data.empty() ? val : std::max(data.top().second, val)});
+    }
+    void pop() {
+        if (!data.empty()) {
+            data.pop();
+        }
+    }
+    int max() { return data.empty() ? 0 : data.top().second; }
+    int size() { return data.size(); }
+    bool empty() { return data.empty(); }
+    int top() { return data.top().first; }
+};
+
+class queue_with_max {
+private:
+    std::queue<int> data;
+    stack_with_max left, right;
+    int window_size;
+public:
+    queue_with_max(int window_size_): window_size(window_size_) {}
+    bool empty() { return data.empty(); }
+    void push(int val) {  // push also pops if window size is exceeded
+        data.push(val);
+        if (left.size() == window_size) {
+            while (!left.empty()) {
+                right.push(left.top());
+                left.pop();
+            }
+        }
+        left.push(val);
+        right.pop();
+        if (data.size() > window_size) {
+            data.pop();
+        }
+    }
+    std::optional<int> max() {
+        if (data.size() < window_size) {
+            return {};
+        }
+        return std::max(left.max(), right.max());
+    }
+};
+
+void cout_optional_if_has_value(const std::optional<int>& opt) {
+    if (opt.has_value()) {
+        std::cout << opt.value() << " ";
+    }
+}
+
+int main() {
+    int len;
+    std::cin >> len;
+    std::vector<int> data(len);
+    for (int i = 0; i < data.size(); ++i) {
+        std::cin >> data[i];
+    }
+    int w_size;
+    std::cin >> w_size;
+    queue_with_max q_m(w_size);
+    cout_optional_if_has_value(q_m.max());
+    for (int i = 0; i < data.size(); ++i) {
+        q_m.push(data[i]);
+        cout_optional_if_has_value(q_m.max());
+    }
+    std::cout << std::endl;
+    return 0;
+}
